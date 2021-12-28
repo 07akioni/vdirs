634272249ef98a9944f6aef3900a7fe509841e33
07akioni <07akioni2@gmail.com>
2021-12-29 01:10:07 +0800

    fix(zindexable): do not warn if never enabled

--- src/zindexable/index.ts
+++ src/zindexable/index.ts
@@ -3,7 +3,8 @@ import zIndexManager from './z-index-manager'
 
 interface ZIndexableElement extends HTMLElement {
   '@@ziContext': {
-    enabled?: boolean
+    enabled: boolean
+    initialized: boolean
   }
 }
 
@@ -14,11 +15,13 @@ const zindexable: ObjectDirective<ZIndexableElement, { zIndex?: number, enabled?
   mounted (el, bindings) {
     const { value = {} } = bindings
     const { zIndex, enabled } = value
+    el[ctx] = {
+      enabled: !!enabled,
+      initialized: false
+    }
     if (enabled) {
       zIndexManager.ensureZIndex(el, zIndex)
-    }
-    el[ctx] = {
-      enabled
+      el[ctx].initialized = true
     }
   },
   updated (el, bindings) {
@@ -27,10 +30,12 @@ const zindexable: ObjectDirective<ZIndexableElement, { zIndex?: number, enabled?
     const cachedEnabled = el[ctx].enabled
     if (enabled && !cachedEnabled) {
       zIndexManager.ensureZIndex(el, zIndex)
+      el[ctx].initialized = true
     }
-    el[ctx].enabled = enabled
+    el[ctx].enabled = !!enabled
   },
   unmounted (el, bindings) {
+    if (!el[ctx].initialized) return
     const { value = {} } = bindings
     const { zIndex } = value
     zIndexManager.unregister(el, zIndex)
